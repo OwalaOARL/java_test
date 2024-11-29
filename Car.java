00c864ddf9bcb7ff5cb8f60438d3fcfa1bb86048
OwalaOARL <112147607+OwalaOARL@users.noreply.github.com>
2024-11-29 23:07:12 +0530

    Update Car.java

--- Car.java
+++ Car.java
@@ -4,10 +4,22 @@ public class Car {
     public String name = "Toyota";
     protected String model = "Vitz";
     boolean isStarted = true;
-    private int speed = 60;
+    private int speed = 80;
 
     public void printData(){
         System.out.println(name + " " + model + " " + isStarted + " " + speed);
+
+    }
+    
+}
+public class Animal {
+    public String name = "Dog";
+    protected String type = "Boxer";
+    boolean isStarted = true;
+   
+
+    public void printData(){
+        System.out.println(name + " " + type + " " + isStarted );
 
     }
     
